9e9eec1ae45a084311a0523d0e49a545fcbb15e7
Yo hello <63665090+Yohello1@users.noreply.github.com>
2022-01-19 17:55:45 +0000

    A minor change

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
+    // A change
     JFrame frame = new JFrame("ICS4UC Quad 2");
     frame.setSize(300, 300);
     frame.setLocation(5, 5);
